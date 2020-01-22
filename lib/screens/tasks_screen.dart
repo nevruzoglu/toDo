553191d6f08a2b250f3d5c2a63fa22e1ba68b804
nevruzoglu <nevruzoglu@gmail.com>
2020-01-23 01:03:12 +0300

    Main page design implement

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 30, right: 30, top: 60, bottom: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 30,
+                    child: Icon(
+                      Icons.list,
+                      size: 30,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'toDO',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  Text(
+                    '12 Task',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      topLeft: Radius.circular(30))),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
