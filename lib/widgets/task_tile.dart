7421adb23891bcf86889d25cfeb1eb04152e5a45
nevruzoglu <nevruzoglu@gmail.com>
2020-01-26 14:00:18 +0300

    refactor to stateless

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatefulWidget {
+class TaskTile extends StatelessWidget {
   @override
-  _TaskTileState createState() => _TaskTileState();
-}
-
-class _TaskTileState extends State<TaskTile> {
-  bool isChecked =
-      false; //! Herşey burdaki boola bağlı, parent state den aşağı dağılıyor.
-
-  // void checkboxCallback(bool checkboxState) {   // ! burdaki callback isimsiz hali aşağıda kullanılıyor kısa olarak
-  //   setState(() {
-  //     isChecked = checkboxState;
-  //   });
-  // }
+  final bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,34 +12,13 @@ class _TaskTileState extends State<TaskTile> {
         style: TextStyle(
             decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
-      trailing: TaskCheckbox(
-        checkboxState: isChecked,
-        toggleCheckboxState: (bool checkboxState) {
-          //! callback fonksiyonu kullanımı yukardakinin kısa hali
-          setState(
-            () {
-              isChecked = checkboxState;
-            },
-          );
-        },
-      ),
-    );
-  }
-}
-
-class TaskCheckbox extends StatelessWidget {
-  final bool checkboxState;
-  final Function toggleCheckboxState;
-
-  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
+      trailing:  Checkbox(
       activeColor: Colors.lightBlueAccent,
       value: checkboxState,
       onChanged:
           toggleCheckboxState, //! 1- stateless widget ekran değiştiremediği için, setstateli fonksiyon atanıyor (callback)
     );
+    );
   }
 }
+
