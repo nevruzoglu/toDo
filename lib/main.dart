ba95ab198382a28c9381279eff0d26013d6e33b1
nevruzoglu <nevruzoglu@gmail.com>
2020-01-27 00:20:08 +0300

    provider builder implement start

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:todo/screens/tasks_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: TasksScreen(),
+    return ChangeNotifierProvider(
+      child: MaterialApp(
+        home: TasksScreen(),
+      ),
     );
   }
 }
